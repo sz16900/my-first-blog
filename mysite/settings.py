8f5b6c827891e355d3b57b972982b4eceef67692
Seth <sz16900@my.bristol.ac.uk>
2017-11-02 18:36:17 -0600

    pythoneverywhere.com bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8k+jmcqq!1ts^wsv2_$#%of!vjwwph_!mje&#g_6p#f)1ro8cd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sz16900.pythonanywhere.com']
 
 
 # Application definition
